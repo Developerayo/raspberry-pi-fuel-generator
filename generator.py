ca0694f03478431f0402d3557c7c13dcd0f25ad6
Shodipo Ayomide <shodipovi@gmail.com>
2022-12-22 00:41:31 +0100

    chore: create function to turn off generator

--- generator.py
+++ generator.py
@@ -12,9 +12,23 @@ pi.set_mode(RELAY_GPIO_PIN, pigpio.OUTPUT)
 
 pi.write(RELAY_GPIO_PIN, 0)
 
-
 def turn_on_generator():
     pi.write(RELAY_GPIO_PIN, 1)
+
+
+def turn_off_generator():
+    pi.write(RELAY_GPIO_PIN, 0)
+
+
+def button_press_handler(channel):
+    if pi.read(RELAY_GPIO_PIN) == 0:
+        turn_on_generator()
+    else:
+        turn_off_generator()
+
+GPIO.add_event_detect(
+    RECEIVER_GPIO_PIN, GPIO.FALLING, callback=button_press_handler, bouncetime=300
+)
 
 while True:
     pass
